ef07fd7d9785310261bad477f8276b25fd7f51a8
JeanGuillaume Vicente <jg.vicente@wanadoo.fr>
2022-03-28 16:03:55 +0200

    booking and review models done

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_28_130249) do
+ActiveRecord::Schema.define(version: 2022_03_28_140231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.bigint "user_id", null: false
+    t.bigint "castle_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["castle_id"], name: "index_bookings_on_castle_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "castles", force: :cascade do |t|
     t.string "name"
@@ -24,6 +35,15 @@ ActiveRecord::Schema.define(version: 2022_03_28_130249) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_castles_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "comment"
+    t.integer "rating"
+    t.bigint "castle_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["castle_id"], name: "index_reviews_on_castle_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,5 +58,8 @@ ActiveRecord::Schema.define(version: 2022_03_28_130249) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "castles"
+  add_foreign_key "bookings", "users"
   add_foreign_key "castles", "users"
+  add_foreign_key "reviews", "castles"
 end
