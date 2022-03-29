9da5d805cbfc1be5f648c5fe463b36625c59bf5d
sebastien clavel <sebclav@hotmail.com>
2022-03-29 16:20:05 +0200

    controller new et create - policy view booking

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,53 @@
+class BookingsController < ApplicationController
+  before_action :set_castle, only: [:new, :create]
+  before_action :set_booking, except: [:index]
+    
+  def index
+    @bookings = Booking.all
+  end
+
+  def new
+    @booking = Booking.new
+    authorize @castle
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+#    @user = User.find(params[:user_id])
+    @booking.castle = @castle
+    @booking.user = current_user
+    if @booking.save
+      redirect_to  castle_booking_path(@booking)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+    authorize @booking
+  end
+
+  def destroy
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :user_id, :castle_id)
+  end
+
+  def set_castle
+    @castle = Castle.find(params[:castle_id])
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+end
--- app/models/castle.rb
+++ app/models/castle.rb
@@ -1,6 +1,7 @@
 class Castle < ApplicationRecord
   belongs_to :user
   has_many :reviews
+  has_many :bookings
   has_one_attached :photo
   validates :name, uniqueness: true
   validates :name, presence: true
