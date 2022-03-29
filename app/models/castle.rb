ae631c32e4d38d0ba57946c75bab8a6dd6f489d4
JeanGuillaume Vicente <jg.vicente@wanadoo.fr>
2022-03-29 10:46:28 +0200

    view castle fixed

--- app/models/castle.rb
+++ app/models/castle.rb
@@ -1,4 +1,9 @@
 class Castle < ApplicationRecord
   belongs_to :user
+  has_many :reviews
   has_one_attached :photo
+  validates :name, uniqueness: true
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :price, presence: true
 end
