302afc8a957fb85b998a50fbf9c1c6969db62283
yoann touffet <yoann.touffet@gmail.com>
2022-03-31 15:45:36 +0200

    reviews form ok

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :castle
 
-  # validates :rating, numericality: { in: 1..5 }
+  validates :comment, presence: true
+  validates :rating, presence: true
+  # validates :rating, numericality: { less_than_or_equal_to: 5, only_integer: true }
 end
